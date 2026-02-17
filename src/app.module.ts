46b580662d52b9865d3e017b2244452fd0123f2f
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-02-17 11:51:07 -0500

    conection a bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    
+   // permite leer globalmete las variables de entorno
+    ConfigModule.forRoot({
+    isGlobal:true
+    }),
+
+    // acceder a la configuracion de typeorm de manera asincrona y usando el servicio de configuracion para obtener las variables de entorno(configService)
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
