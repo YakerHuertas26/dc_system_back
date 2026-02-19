78ea0ac23a0c9b608afd60b2017fafd654747b6a
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-02-18 19:30:11 -0500

    cration entity

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,27 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn({name:'category_id'})
+    id: number;
+
+    @Column({
+        type:'varchar', 
+        length:4, 
+        unique: true
+    })
+    code: string;
+
+    @Column({
+        type:'varchar', 
+        length:4, 
+        unique: true
+    })
+    name: string;
+
+    @Column({
+        type:'boolean',
+        default: true
+    })
+    state: boolean;
+}
