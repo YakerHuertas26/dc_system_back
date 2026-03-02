5252ccad2124fc6afc08442d60cd8b26466fea0e
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-03-02 12:04:13 -0500

    validation dto and service  create categories

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,11 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+      private readonly categoryRepository: Repository <Category>
+  ){}
+
+  // create a new category 
+  async create(createCategoryDto: CreateCategoryDto)  {
+    try {
+      // verificar si el nombre existe
+        const existName= await  this.categoryRepository.exists({
+          where:{name: createCategoryDto.name}
+        })
+
+        if (existName) {
+          throw new ConflictException('El nombre de la categoría ya existe');
+        }
+
+      // verificar si el código existe
+        const existCode = await this.categoryRepository.exists({
+          where:{code: createCategoryDto.code}
+        });
+
+        if (existCode) {
+          throw new ConflictException('El código ya existe')
+        }
+
+
+    } catch (error) {
+      
+    }
   }
 
   findAll() {
