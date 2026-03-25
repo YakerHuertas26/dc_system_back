5340eebfba03aa71d9b92c029fbbb509f4ee3a81
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-03-24 22:22:33 -0500

    init test categories

--- src/modules/categories/categories.service.spec.ts
+++ src/modules/categories/categories.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Categories } from './entities/categories.entity';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
 
+  const mockCategoryRepository= {
+    exists: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneby: jest.fn(),
+  }
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [CategoriesService,{ provide : getRepositoryToken(Categories), useValue: mockCategoryRepository }],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
