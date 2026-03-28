f37713cd45fa54f90799d907134171de46dd4ccf
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-03-28 12:06:05 -0500

    init test controller-categories

--- src/modules/categories/categories.controller.spec.ts
+++ src/modules/categories/categories.controller.spec.ts
@@ -2,16 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 
+const mockCategoriesService = {
+  create:  jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update:  jest.fn(),
+  active:  jest.fn(),
+  remove:  jest.fn(),
+};
+
+const mockCategory = {
+  category_id: 1,
+  name:        'Electrónica',
+  code:        '0001',
+  state:       true,
+};
+
+
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
-      providers: [CategoriesService],
+      providers: [{
+        provide: CategoriesService,
+        useValue: mockCategoriesService,
+      }],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
