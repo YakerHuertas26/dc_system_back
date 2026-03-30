8551b29066ce0c19a74bdbdfd84570d458ddab8f
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-03-30 11:21:43 -0500

    entity and dto product state

--- src/modules/product_states/dto/create-product_state.dto.ts
+++ src/modules/product_states/dto/create-product_state.dto.ts
@@ -1,1 +1,8 @@
-export class CreateProductStateDto {}
+import { IsNotEmpty, IsString, Length,  } from "class-validator";
+
+export class CreateProductStateDto {
+    @IsNotEmpty({message: 'El nombre del estado del estado del producto es requerido'})
+    @IsString({message: 'El nombre del estado del producto debe ser una cadena de texto'})
+    @Length(1,45,{message: 'el nombre del estado del producto debe tener entre 1 y 45 caracteres'})
+    name: string;
+}
