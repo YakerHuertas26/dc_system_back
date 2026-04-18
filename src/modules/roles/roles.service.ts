55d32a5fa9aff6737f7e4599a4c0a4bfb3805e7a
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-04-18 13:10:34 -0500

    [feat]: finOne, update, remove rol

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,9 +1,9 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, ConflictException, HttpException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 
 @Injectable()
 export class RolesService {
@@ -30,21 +30,46 @@ export class RolesService {
     }
   }
 
-  findAll() {
-    return this.roleRepository.find({
+  async findAll() {
+    return await this.roleRepository.find({
       order:{roleId:'DESC'}
     });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    try {
+      const rol= await this.roleRepository.findOneBy({roleId:id})
+      if (!rol) {
+        throw new NotFoundException('el id del rol no existe')
+      }
+      return rol
+    } catch (error) {
+      if (error instanceof HttpException) throw error
+      throw new InternalServerErrorException('Error al obtener los roles')
+    }
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    try {
+      const rol = await this.findOne(id)
+      if (rol.name===updateRoleDto.name) throw new ConflictException('no hay datos por actualizar')
+      const existRol= await this.roleRepository.exists({
+        where:{name:updateRoleDto.name , roleId: Not(id)}
+      })
+      if (existRol) throw new ConflictException('El rol ya existe')
+
+      Object.assign(rol, updateRoleDto);
+
+      return this.roleRepository.save(rol);
+    } catch (error) {
+      if(error instanceof HttpException) throw error
+      throw new InternalServerErrorException('Error al actualizar un rol')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    const rol= await this.findOne(id)
+    this.roleRepository.remove(rol)
+    return `Rol ha sido elimado`;
   }
 }
