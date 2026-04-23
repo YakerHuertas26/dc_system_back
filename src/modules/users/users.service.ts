bfeafc4f9d3ca1932c6e7f3354cb0e92cb8ea467
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2026-04-23 16:51:51 -0500

    [feat]: finish module user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,9 @@
-import { ConflictException, HttpException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, HttpException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { RolesService } from '../roles/roles.service';
 
 @Injectable()
@@ -43,15 +43,54 @@ export class UsersService {
     })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user =await this.userRepository.findOne({
+      where:{userId:id},
+      relations:{
+        role:true
+      }
+    })
+    if (!user) throw new NotFoundException('El usuario no ha sido encontrado')
+    
+    return user;
+
+    } catch (error) {
+      if (error instanceof HttpException) throw error; 
+      throw new InternalServerErrorException('Error al obtener usuario');
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const user= await this.findOne(id);
+        if (user === updateUserDto) throw new ConflictException('No hay datos para actualizar');
+
+        if(updateUserDto.email){
+          const existEmail= await this.userRepository.exists({where:{
+            email:updateUserDto.email, userId: Not(id)
+          }})
+          if (existEmail) throw new ConflictException('El correo electrónico ya está registrado');
+        }
+        if (updateUserDto.roleId) {
+          const role = await this.roleService.findOne(updateUserDto.roleId);
+          user.role= role
+        }
+        Object.assign(user, updateUserDto);
+        const saveUser= await this.userRepository.save(user);
+        const {password, ...result}= saveUser
+        return result;
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Error al actualizar usuario');
+
+    }
+    
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user= await this.findOne(id);
+    user.state= false;
+    return await this.userRepository.save(user);
   }
 }
