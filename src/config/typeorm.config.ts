1053bf99ab39e98953b14cfe77d2d853857e653e
YakerHuertas26 <huertas2602@gmail.com>
2026-04-30 18:16:51 -0500

    [fix]: corection config database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,9 +4,9 @@ import type { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig = (configService: ConfigService):TypeOrmModuleOptions => ({
         type: 'mysql',
         host: configService.get('DB_HOST')|| 'localhost',
-        port: configService.get('DB_PORT') || 3360,
+        port: configService.get('DB_PORT') || 3306,
         username: configService.get('DB_USER') || 'root',
-        password: configService.get('DB_PASSWORD')||'' ,
+        password: configService.get('DB_PASSWORD')||'arthur' ,
         database: configService.get('DB_NAME') || 'dc_system',
         autoLoadEntities: true,
         // entities: [__dirname + '/../**/*.entity{.ts,.js}',],
