9c8192858ce9a76d91d868c95c8b5be40a22c1bf
YakerHuertas26 <huertas2602@gmail.com>
2026-05-06 10:05:33 -0500

    [feat]: create decorator roles

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,24 @@
-
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    // Verificar si la ruta tiene el decorador @Public()
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+     // Si es pública, permitir acceso sin autenticación
+    if (isPublic) {
+      return true;
+    }
+    // Si no es pública, validar JWT
+    return super.canActivate(context);
+  }
+}
